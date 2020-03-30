5e59b99f3390b3eda178d32a9a2b6eb192bfde1e
Deepak HB <deepakhb2@gmail.com>
2020-03-30 22:47:47 +0100

    Queue data structures using linked list.

---
+++ Data-Structures-Queue/queue.rb
@@ -0,0 +1,71 @@
+# Queues using ruby LinkedList 
+#
+class Node
+  attr_reader :value
+  attr_accessor :next
+
+  def initialize(value)
+    @value = value
+    @next = nil
+  end
+end
+
+class Queue
+  attr_reader :length
+
+  def initialize
+    @front, @rear = nil, nil
+    @length = 0
+  end
+
+  def enqueue(value)
+    new_node = Node.new(value)
+    unless(@rear)
+      @front = @rear = new_node
+      @length += 1
+    else
+      @rear.next = new_node 
+      @rear = new_node
+      @length += 1
+    end
+  end
+
+  def dequeue 
+    if(@front)
+      temp_node = @front
+      @front = temp_node.next
+      temp_node.next = nil      
+      @length -= 1
+    else
+      p 'Queue is empty'
+    end
+    temp_node.value
+  end
+
+  def values
+    values = []
+    temp = @front
+    while(temp != nil)
+      values.append(temp.value)
+      temp = temp.next
+    end
+    values
+  end
+
+  def peek
+    @front.value
+  end
+  
+  def isEmpty?
+    @length == 0
+  end
+end
+
+queue = Queue.new
+queue.enqueue(1)
+queue.enqueue(2)
+queue.enqueue(3)
+p queue.dequeue
+p queue.dequeue
+p queue.peek
+p "Length: #{queue.length}"
