f8350b2e6fb7629db9d7f2f6c9ad328ab6c89662
Deepak HB <deepakhb2@gmail.com>
2020-08-21 23:20:31 +0100

    Refactor insertion sort and add rspec tests.

--- lib/Algorithm-Sorting/insertion_sort.rb
+++ lib/Algorithm-Sorting/insertion_sort.rb
@@ -1,39 +1,26 @@
-values = [5,2,4,7,1,3,6]
-print "Input: "
-p values
+class InsertionSort
+  class << self
+    # Sort using recursion
+    def recursive(values, i=0)
+      return values if i >= values.size
 
-# Sort using loop
-def l_insertion_sort(values)
-  n = values.size
-  (0...n).each do |i|
-    (0...i).each do |j|
-      if(values[i] < values[j])
-        values[j], values[i] = values[i], values[j]
+      (0...i).each do |j|
+        values[j], values[i] = values[i], values[j] if (values[i] < values[j])
       end
+      return recursive(values, i+1)
+    end
+
+    # Sort using loop
+    def non_recursive(values)
+      n = values.size
+      (0...n).each do |i|
+        (0...i).each do |j|
+          if(values[i] < values[j])
+            values[j], values[i] = values[i], values[j]
+          end
+        end
+      end
+      return values
     end
   end
-  return values
 end
-
-print "Result: "
-p l_insertion_sort(values)
-
-values = [5,2,4,7,1,3,6]
-
-# Sort using recursion
-def r_insertion_sort(values, i=0)
-  if(i >= values.size)
-    return values
-  end
-
-  (0...i).each do |j|
-    if(values[i] < values[j])
-      values[j], values[i] = values[i], values[j]
-    end
-  end
-  return r_insertion_sort(values, i+1)
-end
-
-print "Result: "
-p r_insertion_sort(values)
-  
