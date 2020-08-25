ddb5b6c25ed5ca2072851015d737d2e966d26232
Deepak HB <deepakhb2@gmail.com>
2020-08-25 23:48:43 +0100

    Add tests for selection sort refactor code.

--- lib/Algorithm-Sorting/selection_sort.rb
+++ lib/Algorithm-Sorting/selection_sort.rb
@@ -1,46 +1,39 @@
 # Time complexity:
 # Omega(n^2)
 # Teta(n^2)
-# 
-values = [5,2,4,7,1,3,6]
-print "Input: "
-p values
-
-# Solve bubble sort using loop
-def l_selection_sort(values)
-  len = values.size
-  (0...len-1).each do |i|
-    min_index = i
-    (i...len).each do |j|
-      if(values[j] < values[min_index])
-        min_index = j
+#
+class SelectionSort
+  class << self
+    def recursive(values, i=0)
+      len = values.size
+      if(i >= len)
+        return values
       end
+      j = i+1
+      min_index = i
+      while(j < len)
+        if(values[j] < values[min_index])
+          min_index = j
+        end
+        j += 1
+      end
+      values[i], values[min_index] = values[min_index], values[i]
+      return recursive(values, i+1)
     end
-    values[i], values[min_index] = values[min_index], values[i]
-  end
-  return values
-end
 
-print "Result: "
-p l_selection_sort(values)
-
-values = [5,2,4,7,1,3,6]
-def r_selection_sort(values, i)
-  len = values.size
-  if(i >= len)
-    return values
-  end
-  j = i+1
-  min_index = i
-  while(j < len)
-    if(values[j] < values[min_index])
-      min_index = j
+    # Solve bubble sort using loop
+    def non_recursive(values)
+      len = values.size
+      (0...len-1).each do |i|
+        min_index = i
+        (i...len).each do |j|
+          if(values[j] < values[min_index])
+            min_index = j
+          end
+        end
+        values[i], values[min_index] = values[min_index], values[i]
+      end
+      return values
     end
-    j += 1
   end
-  values[i], values[min_index] = values[min_index], values[i]
-  return r_selection_sort(values, i+1)
 end
-
-print "Result: "
-p r_selection_sort(values, 0)
---
+++ spec/Algorithm-Sorting/seletion_sort_spec.rb
@@ -0,0 +1,6 @@
+require_relative './shared_example.rb'
+require_relative '../../lib/Algorithm-Sorting/selection_sort.rb'
+
+describe SelectionSort do
+  include_examples "sorting"
+end
